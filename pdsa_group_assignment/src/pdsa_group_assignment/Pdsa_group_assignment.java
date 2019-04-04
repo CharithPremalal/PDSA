2b2215e7f7aea7fab657d538f49b009310048750
AAbenayaka <cobsccomp182p-008@student.nibm.lk>
2019-04-04 22:38:24 +0530

    Create Pdsa_group_assignment.java

---
+++ pdsa_group_assignment/src/pdsa_group_assignment/Pdsa_group_assignment.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pdsa_group_assignment;
+
+/**
+ *
+ * @author Yanik Perera
+ */
+public class Pdsa_group_assignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
